9a8af76289a753c234531bb897f1a22206414330
Majid <majid3585@gmail.com>
2021-12-20 18:05:32 +1100

    Test is OK. Main functionality of the robot code are OK.

--- robot.rb
+++ robot.rb
@@ -1,6 +1,62 @@
 class Robot
-  def add(a,b)
-    return a,b,a+b
+  @@directions = [:NORTH,:EAST,:SOUTH,:WEST]
+  @@moveCode = {
+    :NORTH => [0,+1],
+    :EAST => [+1,0],
+    :SOUTH => [0,-1],
+    :WEST => [-1,0]
+
+  }
+  def initialize(dimX=5, dimY=5)
+    @dimX = dimX
+    @dimY = dimY
   end
+
+  def PLACE(x =0, y =0, f=:NORTH)
+    if f.class == String
+      f = f.to_sym
+    end
+    @x= x
+    @y= y
+    @f= f
+  end
+
+  def MOVE()
+    tempX = @x+ @@moveCode[@f][0]
+    tempY = @y+ @@moveCode[@f][1]
+    if tempX >=0 && tempX < @dimX && tempY>=0 && tempY < @dimY
+      @x = tempX
+      @y = tempY
+    end
+  end
+
+  def LEFT()
+    newDirID = (@@directions.index(@f)-1) % @@directions.length
+    @f = @@directions[newDirID]
+  end
+
+  def RIGHT()
+    newDirID = (@@directions.index(@f)+1) % @@directions.length
+    @f = @@directions[newDirID]
+  end
+
+  def REPORT()
+    return @x,@y,@f.to_s
+  end
+
+  def prepareCommand(command)
+    # command = "r." + command.reverse.sub(",", ",:".reverse).reverse
+    command = command.sub("NORTH",":NORTH").sub("EAST",":EAST").sub("SOUTH",":SOUTH").sub("WEST",":WEST")
+    return command
+  end
+
 end
 
+# r= Robot.new()
+# r.PLACE(1,2,'EAST')
+# # eval("r."+r.prepareCommand("PLACE 1,2,EAST"))
+# r.MOVE
+# r.MOVE
+# r.LEFT
+# r.MOVE
+# p r.REPORT()
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -7,11 +7,26 @@ RSpec.describe "Robot" do
     puts testCase
     it "start of testing" + testCase  do
       r = Robot.new
-      t = r.add(3,4)
-      expect(t).to eq([3,4,7])
+      finished = false
+      testResult = "-"
+      desiredResult = "+"
       File.readlines(testCase).each do |command|
-        puts command.chop
+        if finished
+          command = "[" + r.prepareCommand(command) + "]"
+          desiredResult = eval(command)
+          desiredResult[-1] = desiredResult[-1].to_s
+          break
+        end
+        if command.start_with?("PLACE")
+          eval("r."+r.prepareCommand(command.chop))
+        elsif command.start_with?("MOVE") || command.start_with?("LEFT") || command.start_with?("RIGHT")
+          eval("r."+command.chop)
+        elsif command.start_with?("REPORT")
+          testResult =  eval("r."+command.chop)
+          finished = true
+        end
       end
+      expect(testResult).to eq(desiredResult)
     end
   }
 end
